728c99ee64a3abc9456178856090d04e5183f75d
Rafael Neto <5102522+rafaeelneto@users.noreply.github.com>
2019-08-21 12:16:50 -0300

    Dashboard layout and implement the map plataform

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,21 @@ document.querySelector('#sidebarCollapse').addEventListener('click', function(){
 });
 
 //-1.41501/-48.42868
+
+/*
 const map = L.map('map').setView([-1.41, -48.42], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/rafaeel/cjz8tael002on1dqclsqt84ik/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmFmYWVlbCIsImEiOiJjano4dDh1OHMwM2xtM2JwbG9jcWFmaXhzIn0.SihsXFXEW54INSxYcZ6_BQ', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.light',
 	accessToken: 'pk.eyJ1IjoicmFmYWVlbCIsImEiOiJjano4dDh1OHMwM2xtM2JwbG9jcWFmaXhzIn0.SihsXFXEW54INSxYcZ6_BQ'
 }).addTo(map);
 
-map.zoomControl.setPosition('bottomright');
+map.zoomControl.setPosition('bottomright');*/
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFmYWVlbCIsImEiOiJjano4dDh1OHMwM2xtM2JwbG9jcWFmaXhzIn0.SihsXFXEW54INSxYcZ6_BQ';
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/rafaeel/cjz8tael002on1dqclsqt84ik'
+});
