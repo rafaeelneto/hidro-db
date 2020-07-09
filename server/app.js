10f6f95f2c1714a3d5511acb54ec91bbd1812722
Rafael Neto <5102522+rafaeelneto@users.noreply.github.com>
2020-07-09 15:03:30 -0300

    Refactoring

---
+++ server/app.js
@@ -0,0 +1,134 @@
+//Config the enviroment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+//Make the main imports
+const bodyParser = require("body-parser");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const db = require("./database/db");
+
+const users = require("./models/Users");
+
+const dashboard = require("./routes/dashboard");
+
+//Configure the express extension
+const app = express();
+
+//app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
+
+//Import routers
+
+//Initialize extension
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  session({
+    key: "user_sid",
+    secret: "YUBnçsiuaSIDSFOjadiywghdjshADShaA",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie("user_sid");
+  }
+  next();
+});
+
+//Check for logged users
+const sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.redirect("/dashboard");
+  } else {
+    next();
+  }
+};
+
+const passTo = (req, res, username) => {
+  req.session.user = [username];
+  res.redirect("/dashboard");
+};
+
+//The index and login acess
+app
+  .route("/")
+  .get(sessionChecker, (req, res, next) => {
+    res.sendFile(__dirname + "/public/index.html");
+  })
+  .post(sessionChecker, async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (username == users.visitante) {
+      password = username;
+    }
+
+    const isValid = await users.autheticateUser(username, password);
+
+    if (isValid) {
+      passTo(req, res, username);
+    } else {
+      res.send("erroooou");
+    }
+  });
+
+app.route("/usr/create").post(async (req, res) => {
+  let usernameAdmin = req.body.usernameAdmin;
+  let passwordAdmin = req.body.passwordAdmin;
+  let username = req.body.username;
+  let password = req.body.password;
+
+  const isValid = await users.autheticateUser(usernameAdmin, passwordAdmin);
+  if (isValid) {
+    const sucess = await users.insertNewUser(
+      usernameAdmin,
+      passwordAdmin,
+      username,
+      password
+    );
+    if (sucess === true) {
+      const isValid = await users.autheticateUser(username, password);
+      passTo(req, res, username);
+    } else {
+      res.send("Usuário já existente");
+    }
+  } else {
+    res.send("Administrador inválido");
+  }
+});
+
+//Server the static files of the public folder
+app.use("/", express.static("./public/"));
+
+//Access to the dashboard
+app.use("/dashboard", dashboard);
+
+//Logout
+app.get("/logout", (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.clearCookie("user_sid");
+    users.closeDB();
+  }
+  res.redirect("/");
+});
+
+//Initialize the server on the enviroment port
+app.listen(process.env.PORT || 8081, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
---
+++ server/database/db.js
@@ -0,0 +1,55 @@
+const { Pool } = require("pg");
+
+const connectionString = `postgres://postgres:26119499@localhost/uema`;
+const connectionAuthString = `postgres://postgres:26119499@localhost/uema`;
+
+//const connectionString = `postgres://uema_publico:uemapublico@10.20.100.30:5432/uema`;
+//const connectionAuthString = `postgres://uema_user:uema2611@10.20.100.30:5432/uema`;
+
+const pool = new Pool({
+  connectionString: connectionString,
+});
+
+const querySelect = function (queryText, param) {
+  //pool.query(queryText, param)
+};
+
+module.exports = {
+  setUsr: (usr, psw) => {
+    /*
+        this.poolUsr = new Pool();
+
+        if(usr !== 'visitante'){
+            const connectionString = `postgres://${usr}:${psw}@35.193.155.132:5432/cosanpa-db`
+            this.poolUsr = new Pool({
+                connectionString: connectionString
+            });
+        }else{
+            this.poolUsr = new Pool();
+        }*/
+  },
+  getParam: () => {
+    return pool.connect();
+  },
+  query: (text, params) => pool.query(text, params),
+  queryAuth: (text, params) => {
+    let poolAuth = new Pool();
+
+    if (usr !== "visitante") {
+      poolAuth = new Pool({
+        connectionString: connectionAuthString,
+      });
+    } else {
+      poolAuth = new Pool();
+    }
+
+    poolAuth.query(text, params);
+    poolAuth.end();
+  },
+  closeDB: () => {
+    if (this.poolUsr) {
+      this.poolUsr.end();
+    }
+    console.log("User connection closed");
+  },
+};
